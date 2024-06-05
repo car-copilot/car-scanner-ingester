6886440365f56f1ef833673e69a1da5afc52aec3
obito1903 <sam200133@gmail.com>
2024-06-05 17:26:20 +0200

    refactor & http server

---
+++ pkg/pg.go
@@ -0,0 +1,72 @@
+package obd2influx
+
+import (
+	"database/sql"
+	"strings"
+	"time"
+
+	"github.com/rs/zerolog/log"
+)
+
+func ConnectPg() {
+	var err error
+	db, err = sql.Open("postgres", K.MustString("pg.connection"))
+	if err != nil {
+		log.Fatal().Err(err)
+	}
+}
+
+func registerTrip(vehicle Vehicle, owner string, start time.Time, end time.Time) string {
+	// Check if user exists
+	var userId int
+	var bucket string
+	err := db.QueryRow("SELECT owner_id, bucket FROM owners WHERE name = $1", owner).Scan(&userId, &bucket)
+	if err != nil {
+		log.Err(err).Send()
+		bucket = strings.ToLower(owner)
+		log.Info().Msgf("Owner %s notfound", owner)
+		log.Info().Msgf("Creating owner %s with bucket %s", owner, bucket)
+		// Create user
+		_, err := db.Exec("INSERT INTO owners (name, bucket) VALUES ($1, $2)", owner, bucket)
+		if err != nil {
+			log.Fatal().Err(err)
+		}
+		err = db.QueryRow("SELECT id FROM owners WHERE name = $1", owner).Scan(&userId, &bucket)
+		if err != nil {
+			log.Fatal().Err(err)
+		}
+	}
+
+	// Check if vehicle exists
+	var vehicleId int
+	err = db.QueryRow("SELECT car_id FROM car WHERE brand = $1 AND model = $2 AND year = $3 AND engine = $4", vehicle.Brand, vehicle.Model, vehicle.Year, vehicle.Engine).Scan(&vehicleId)
+	if err != nil {
+		log.Err(err).Send()
+		log.Info().Msgf("Vehicle %s %s %d %s not found", vehicle.Brand, vehicle.Model, vehicle.Year, vehicle.Engine)
+		log.Info().Msgf("Creating vehicle %s %s %d %s", vehicle.Brand, vehicle.Model, vehicle.Year, vehicle.Engine)
+		// Create vehicle
+		_, err := db.Exec("INSERT INTO car (brand, model, year, engine) VALUES ($1, $2, $3, $4)", vehicle.Brand, vehicle.Model, vehicle.Year, vehicle.Engine)
+		if err != nil {
+			log.Fatal().Err(err)
+		}
+		err = db.QueryRow("SELECT id FROM car WHERE brand = $1 AND model = $2 AND year = $3 AND engine = $4", vehicle.Brand, vehicle.Model, vehicle.Year, vehicle.Engine).Scan(&vehicleId)
+		if err != nil {
+			log.Fatal().Err(err)
+		}
+	}
+
+	// Check if trip exists
+	var tripId int
+	err = db.QueryRow("SELECT trip_id FROM trip WHERE begin_timestamp = $1 AND end_timestamp = $2 AND car_id = $3 AND owner_id = $4", start, end, vehicleId, userId).Scan(&tripId)
+	if err == nil {
+		log.Info().Msgf("Trip already registered with id %d", tripId)
+		return bucket
+	}
+
+	// Register trip
+	_, err = db.Exec("INSERT INTO trip (begin_timestamp, end_timestamp, car_id, owner_id) VALUES ($1, $2, $3, $4)", start, end, vehicleId, userId)
+	if err != nil {
+		log.Fatal().Err(err)
+	}
+	return bucket
+}
---
+++ pkg/process.go
@@ -0,0 +1,85 @@
+package obd2influx
+
+import (
+	"context"
+	"io"
+	"io/fs"
+	"math"
+	"os"
+	"path"
+	"time"
+
+	"github.com/influxdata/influxdb-client-go/v2/api/write"
+	"github.com/rs/zerolog/log"
+)
+
+func ProcessCsv(reader io.Reader, date time.Time, vehicle Vehicle, mergedPids map[string]string, mergedIgnorePids []string, owner string, interval time.Duration) {
+	raw_points, endDate := ReadCsv(reader, date, mergedPids, mergedIgnorePids, vehicle.Convert)
+
+	log.Info().Msgf("Registering trip in database")
+	bucket := registerTrip(vehicle, owner, date, endDate)
+
+	points := GroupDataPoint(raw_points, interval)
+	influxPoints := []*write.Point{}
+	for _, point := range points {
+		influxPoints = append(influxPoints, point.ToInfluxPoint())
+	}
+	if *Config.ListPids {
+		mapPids := make(map[string]bool)
+		for _, point := range points {
+			if _, ok := mapPids[point.Pid]; !ok {
+				mapPids[point.Pid] = true
+				log.Info().Msgf("%s | %s", point.Pid, point.Unit)
+			}
+		}
+	} else {
+		if !*Config.DryRun {
+			deleteAPI := influxClient.DeleteAPI()
+			orgObj, err := influxClient.OrganizationsAPI().FindOrganizationByName(context.Background(), Config.Influx.Org)
+			if err != nil {
+				log.Fatal().Err(err)
+			}
+			bucketObj, err := influxClient.BucketsAPI().FindBucketByName(context.Background(), bucket)
+			if err != nil {
+				log.Fatal().Err(err)
+			}
+			if bucketObj == nil {
+				log.Info().Msgf("Creating bucket %s in org %s", bucket, orgObj.Name)
+
+				bucketObj, err = influxClient.BucketsAPI().CreateBucketWithName(context.Background(), orgObj, bucket)
+				if err != nil {
+					log.Fatal().Err(err)
+				}
+			}
+
+			log.Info().Msgf("Deleting data from %s to %s for bucket %s in org %s", date, endDate, bucketObj.Name, orgObj.Name)
+			deleteAPI.Delete(context.Background(), orgObj, bucketObj, date, endDate, "_measurement=\"*\"")
+			writeAPI := influxClient.WriteAPIBlocking(Config.Influx.Org, bucket)
+			log.Info().Msgf("Writing %d points to InfluxDB", len(points))
+			log.Info().Msgf("Data available at https://car-grafana.obito.fr/d/edmyb3fp6zocge/car-scanner?orgId=1&var-car=%s&from=%d&to=%d&refresh=10s", bucket, date.UnixMilli(), endDate.UnixMilli())
+			for i, point := range influxPoints {
+				progress := math.Round(float64(i) / float64(len(influxPoints)) * 100)
+				modulo := (i % (len(influxPoints) / 100))
+				if modulo == 0 {
+					log.Info().Msgf("Progress: %.2f%%", progress)
+				}
+				if err := writeAPI.WritePoint(context.Background(), point); err != nil {
+					log.Fatal().Err(err)
+				}
+			}
+		}
+	}
+}
+
+func ProcessFile(file fs.DirEntry, vehicle Vehicle, mergedPids map[string]string, mergedIgnorePids []string, owner string, interval time.Duration) {
+	date, err := time.ParseInLocation("2006-01-02 15-04-05.csv", file.Name(), time.Local)
+	if err != nil {
+		log.Fatal().Err(err)
+	}
+	log.Info().Msgf("Processing file %s", file.Name())
+	reader, err := os.Open(path.Join(vehicle.Path, file.Name()))
+	if err != nil {
+		log.Fatal().Err(err)
+	}
+	ProcessCsv(reader, date, vehicle, mergedPids, mergedIgnorePids, owner, interval)
+}
